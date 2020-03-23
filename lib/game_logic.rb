858ebc4bfe0227a1be678325794724c370440f8a
mariosknl <mariosknl@outlook.com>
2020-03-23 16:01:07 +0200

    check winner

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -29,3 +29,34 @@ class Logic
     return player1 == @@choose_player ? player2 : player1
   end
 end
+
+class Winner
+  def win_moves(cases)
+    player1_array = []
+    player2_array = []
+    winning_cases = [ [0,1,2], [0,4,8], [0,3,6], [1,4,7], [2,5,8], [2,4,6],[3,4,5], [6,7,8] ]
+    player_win = nil
+
+    cases.each_with_index do |index,value|
+      if index == "X"
+        player1_array << value
+      elsif index == "O"
+        player2_array << value
+      end
+    end
+
+    winning_cases.each do |i|
+      if player1_array == i
+        player_win = "Player - 1 wins"
+        break
+      elsif player2_array == i
+        player_win = "Player - 2 Wins"
+        break
+      else
+        nil
+      end
+      # break if player_win
+    end
+      player_win
+  end
+end
