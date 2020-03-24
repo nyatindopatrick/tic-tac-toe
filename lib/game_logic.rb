c6bec6bda66d93dbb0a3b4cd67c3f78f21a8eb2e
Patrick Nyatindo <nyatindopatrick@gmail.com>
2020-03-24 11:21:07 +0300

    refactor logic and winner code

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -1,62 +1,66 @@
+# rubocop:disable Style/GlobalVars
+# Display Board
 class Board
-  # attr_reader :place
+  $place = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 
-  # def initialize(place)
-  #     @place = place
-  # end
-  def display_board(place)
+  def display_board
     puts "\t -------------"
-    puts "\t | #{place[0]} | #{place[1]} | #{place[2]} |"
+    puts "\t | #{$place[0]} | #{$place[1]} | #{$place[2]} |"
     puts "\t -------------"
-    puts "\t | #{place[3]} | #{place[4]} | #{place[5]} |"
+    puts "\t | #{$place[3]} | #{$place[4]} | #{$place[5]} |"
     puts "\t -------------"
-    puts "\t | #{place[6]} | #{place[7]} | #{place[8]} |"
+    puts "\t | #{$place[6]} | #{$place[7]} | #{$place[8]} |"
     puts "\t -------------"
   end
 end
 
+# Game logic
 class Logic
-  @@choose_player = nil
-  @@count = 1
-
-  def players_turn(player1, player2)
-    @@choose_player = rand(0..1)
-    @@choose_player = (@@choose_player == 0 ? player1 : player2)
-    @@choose_player
+  # Prompt user to enter value
+  def askuser(param)
+    user_prompt = "#{param} - Please enter a value between 1-9"
+    puts user_prompt
+    player1_choice = gets.chomp.to_i
+    player1_choice
   end
 
-  def change_player(player1, player2)
-    return player1 == @@choose_player ? player2 : player1
+  def update_board(param1, param2)
+    res = nil
+    if $place[param1 - 1] == ' '
+      $place[param1 - 1] = param2
+    else
+      res = 'Spot unavailable. Please choose another spot.'
+    end
+    res
   end
 end
 
+# Determining the winner
 class Winner
-  def win_moves(cases)
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+  end
+
+  def win_moves
     player1_array = []
     player2_array = []
-    winning_cases = [ [0,1,2], [0,4,8], [0,3,6], [1,4,7], [2,5,8], [2,4,6],[3,4,5], [6,7,8] ]
+    winning_cases = [[0, 1, 2], [0, 4, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8],
+                     [2, 4, 6], [3, 4, 5], [6, 7, 8]]
     player_win = nil
 
-    cases.each_with_index do |index,value|
-      if index == "X"
+    $place.each_with_index do |index, value|
+      if index == 'X'
         player1_array << value
-      elsif index == "O"
+      elsif index == 'O'
         player2_array << value
       end
     end
 
-    winning_cases.each do |i|
-      if player1_array == i
-        player_win = "Player - 1 wins"
-        break
-      elsif player2_array == i
-        player_win = "Player - 2 Wins"
-        break
-      else
-        nil
-      end
-      # break if player_win
-    end
-      player_win
+    player_win = "#{@player1} wins" if winning_cases.include? player1_array
+    player_win = "#{@player2} Wins" if winning_cases.include? player2_array
+
+    player_win
   end
 end
+# rubocop:enable Style/GlobalVars
