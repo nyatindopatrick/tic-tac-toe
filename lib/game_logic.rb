25afe5430c86350ab470eecdf45fe34fea96355e
Patrick Nyatindo <nyatindopatrick@gmail.com>
2020-03-26 11:19:26 +0300

    refactor win move to two mehods

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -22,21 +22,26 @@ class Board
   # Checking and eturning the winner
   def win_moves(player)
     temp = []
-    winning_cases = [[0, 1, 2], [0, 4, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8],
-                     [2, 4, 6], [3, 4, 5], [6, 7, 8]]
-    player_win = nil
     @board.each_with_index do |val, i|
       player == @player1 ? (temp << i if val == 'X') : (temp << i if val == 'O')
     end
-    if temp.length <= 3
-      player_win = "#{player} wins" if winning_cases.include? temp
+    temp
+  end
+
+  def check_winner(player,param2)
+    winning_cases = [[0, 1, 2], [0, 4, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [2, 4, 6], [3, 4, 5], [6, 7, 8]]
+    player_win = nil
+    if param2.length <= 3
+      player_win = "#{player} wins" if winning_cases.include? param2
     else
       winning_cases.each do |j|
-        player_win = "#{player} wins" if (temp + j).uniq.length == 4
+        player_win = "#{player} wins" if (param2 + j).uniq.length == 4
       end
     end
     player_win
   end
+
 
   # updating the board
   def update_board(param1, param2)
